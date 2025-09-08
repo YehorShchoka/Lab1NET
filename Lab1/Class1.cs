0619610a3908501e21c15b7468da0690479b856f
EgorShoka <segorushka@gmail.com>
2025-09-08 17:18:04 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Tabul
+    {
+        public double[,] xy = new double[1000, 2];
+        public int n = 0;
+
+        private double f1(double x)
+        {
+            return Math.Pow(x, 3) - Math.Log(Math.Abs(x) + 1);
+        }
+
+        private double f2(double x)
+        {
+            return ((2 * x) + 2) / (Math.Tan((2 * x) - 1) + 1);
+        }
+
+        private double f3(double x)
+        {
+            return Math.Pow(x, 4) - Math.Pow(x, x);
+        }
+
+        public void tab(double xn = -1.25, double xk = 9.39, double h = 0.1, double a = 0.4)
+        {
+            double x = xn, y;
+            int i = 0;
+            while (x <= xk)
+            {
+                if (x < 0)
+                {
+                    y = f1(x);
+                }
+                else
+                {
+                    if ((x >= 0) && (x < a))
+                    {
+                        y = f2(x);
+                    }
+                    else
+                    { y = f3(x); }
+                }
+                xy[i, 0] = x;
+                xy[i, 1] = y;
+                x = x + h;
+                i++;
+            }
+            n = i;
+        }
+    }
+}
---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            x = new DataGridViewTextBoxColumn();
+            y = new DataGridViewTextBoxColumn();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(37, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(21, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Xn";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(120, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(20, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Xk";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(200, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(14, 15);
+            label3.TabIndex = 2;
+            label3.Text = "h";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(274, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(13, 15);
+            label4.TabIndex = 3;
+            label4.Text = "a";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(133, 76);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Результати";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 38);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(72, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(90, 39);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(72, 23);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(168, 39);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(72, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(246, 38);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(72, 23);
+            textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(324, 38);
+            button1.Name = "button1";
+            button1.Size = new Size(84, 23);
+            button1.TabIndex = 9;
+            button1.Text = "Обрахувати";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { x, y });
+            dataGridView1.Location = new Point(12, 94);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(243, 344);
+            dataGridView1.TabIndex = 10;
+            // 
+            // x
+            // 
+            x.HeaderText = "x";
+            x.Name = "x";
+            // 
+            // y
+            // 
+            y.HeaderText = "y";
+            y.Name = "y";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            chart1.Location = new Point(261, 94);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(527, 344);
+            chart1.TabIndex = 11;
+            chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(chart1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn x;
+        private DataGridViewTextBoxColumn y;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
